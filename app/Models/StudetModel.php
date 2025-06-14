4f8bc99756ce260b425b4cd7058d889d57e3a6cb
Hanas <hanasoke@gmail.com>
2025-06-14 14:07:25 +0700

    update Student Model

--- app/Models/StudetModel.php
+++ app/Models/StudetModel.php
@@ -10,6 +10,28 @@ class StudentModel extends Model
 
     protected $allowedFields = ['name', 'email', 'sn', 'address', 'age', 'faculty'];
 
+    protected $useTimeStamps = true;
+    protected $createField = 'created_at',
+    protected $updatedField = 'updated_at';
+
+    protected $validationRules = [
+        'name' => 'required|min_length[3]',
+        'email' => 'required|valid_email|is_unique[students.email]',
+        'sn' => 'required|is_unique[students.sn]',
+        'age' => 'required|numeric',
+        'faculty' => 'required'
+    ]
+
+    protected $validationMessages = [
+        'email' => [
+            'is_unique' => 'Sorry. That email has already been taken. Please choose another.'
+        ],
+
+        'sn' => [
+            'is_unique' => 'this serial number already exists.'
+        ]
+    ]
+
 }
 
 
